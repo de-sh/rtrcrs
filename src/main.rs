18120cc54ed52f8e1c280d25ce6674bf9d26285f
Devdutt Shenoi <devdutt@outlook.in>
2020-11-19 13:35:08 +0530

    Add vector::Vec3 definition

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
-fn main() {
+use std::io::stderr;
+use std::io::Write;
 
+
+mod vector;
+use vector::Vec3;
+use Vec3 as Point3;
+use Vec3 as Color;
+
+fn main() {
     // Image
 
     const IMAGE_WIDTH: i32 = 256;
@@ -10,9 +18,11 @@ fn main() {
     println!("P3\n{} {}\n255", IMAGE_WIDTH, IMAGE_HEIGHT);
 
     for j in (0..IMAGE_HEIGHT).rev() {
+        eprint!("\rScanlines remaining: {} ", j);
+        stderr().flush().unwrap();
         for i in 0..IMAGE_WIDTH {
-            let r: f64 = (i as f64/ (IMAGE_WIDTH-1) as f64);
-            let g: f64 = (j as f64/ (IMAGE_HEIGHT-1) as f64);
+            let r: f64 = i as f64 / (IMAGE_WIDTH - 1) as f64;
+            let g: f64 = j as f64 / (IMAGE_HEIGHT - 1) as f64;
             let b: f64 = 0.25;
 
             let ir = (255.999 * r) as i32;
@@ -22,4 +32,18 @@ fn main() {
             println!("{} {} {}", ir, ig, ib);
         }
     }
+
+    eprintln!("\rImage Generated.");
+    stderr().flush().unwrap();
+
+    let red = Color::new(0.1, 1.2, 2.3);
+    let green = Color::new(2.3, 1.2, 0.1);
+    println!("Add: {}", red + green);
+    println!("Sub: {}", red - green);
+    println!("Mul: {}", red * 2.0);
+    println!("VecMul: {}", green * red);
+    println!("Div: {}", green / 2.0);
+    println!("Dot: {}", green.dot(red));
+    println!("Cross: {}", green.cross(red));
+    println!("Unit: {}", Color::unit_vector(green));
 }
---
+++ src/vector.rs
@@ -0,0 +1,124 @@
+use std::fmt;
+use std::ops;
+
+#[derive(Copy, Clone)]
+pub struct Vec3 {
+    x: f64,
+    y: f64,
+    z: f64,
+}
+
+impl Vec3 {
+    pub fn new(x: f64, y: f64, z: f64) -> Self {
+        Self { x, y, z }
+    }
+
+    pub fn x(self) -> f64 {
+        self.x
+    }
+    pub fn y(self) -> f64 {
+        self.y
+    }
+    pub fn z(self) -> f64 {
+        self.z
+    }
+
+    pub fn length_squared(self) -> f64 {
+        self.x.powi(2) + self.y.powi(2) + self.z.powi(2)
+    }
+
+    pub fn length(self) -> f64 {
+        self.length_squared().sqrt()
+    }
+
+    pub fn unit_vector(vec: Self) -> Self {
+        vec / vec.length()
+    }
+
+    pub fn dot(self, rhs: Self) -> f64 {
+        self.x * rhs.x + self.y * rhs.y + self.z * rhs.z
+    }
+
+    pub fn cross(self, rhs: Self) -> Self {
+        Self{
+            x: self.y * rhs.z - self.z * rhs.y,
+            y: self.z * rhs.x - self.x * rhs.z,
+            z: self.x * rhs.y - self.y * rhs.x,
+        }
+    }
+}
+
+impl fmt::Display for Vec3 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "x: {}\ty: {}\tz: {}", self.x, self.y, self.z)
+    }
+}
+
+impl ops::Add for Vec3 {
+    type Output = Vec3;
+
+    fn add(self, rhs: Self) -> Self {
+        Self {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+            z: self.z + rhs.z,
+        }
+    }
+}
+
+impl ops::Sub for Vec3 {
+    type Output = Vec3;
+
+    fn sub(self, rhs: Self) -> Self {
+        Self {
+            x: self.x - rhs.x,
+            y: self.y - rhs.y,
+            z: self.z - rhs.z,
+        }
+    }
+}
+
+impl ops::Mul<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: f64) -> Self {
+        Self {
+            x: self.x * rhs,
+            y: self.y * rhs,
+            z: self.z * rhs,
+        }
+    }
+}
+
+impl ops::Mul<Vec3> for f64 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Vec3) -> Vec3 {
+        rhs * self
+    }
+}
+
+/// Mul between two Vec3
+impl ops::Mul<Vec3> for Vec3 {
+    type Output = Vec3;
+
+    fn mul(self, rhs: Self) -> Self {
+        Self{
+            x: self.x * rhs.x,
+            y: self.y * rhs.y,
+            z: self.z * rhs.z,
+        }
+    }
+}
+
+impl ops::Div<f64> for Vec3 {
+    type Output = Vec3;
+
+    fn div(self, rhs: f64) -> Self {
+        Self {
+            x: self.x / rhs,
+            y: self.y / rhs,
+            z: self.z / rhs,
+        }
+    }
+}
