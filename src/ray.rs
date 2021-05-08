bd5b664d07d7a248477b5294d641c02f3792fb83
Jery James Jacob Arangath <31083898+firebreath1001@users.noreply.github.com>
2021-05-08 18:18:57 +0000

    Tried adding sphere definition into ray code (#1)
    * Tried adding sphere definition into ray code
    - perform cargo format
    - write code for sphere if ray intersects
    
    TODO: FIX necessary, improper image generated
    
    * Single lined imports
    
    Co-authored-by: Devdutt Shenoi <devdutt@outlook.in>

--- src/ray.rs
+++ src/ray.rs
@@ -22,8 +22,22 @@ impl Ray {
     }
 
     pub fn color(self) -> Color {
+        if self.hit_sphere(Point3::new(0.0, 0.0, 2.0), 0.01) {
+            return Color::new(1.0, 0.0, 0.0);
+        }
         let unit_dir = Vec3::unit_vector(self.dir);
         let t = 0.5 * (unit_dir.y() + 1.0);
         (1.0 - t) * Color::new(1.0, 1.0, 1.0) + t * Color::new(0.5, 0.7, 1.0)
+    }
+
+    pub fn hit_sphere(&self, center: Point3, radius: f64) -> bool {
+        let oc = self.orig - center;
+        let (a, b, c) = (
+            self.dir.dot(self.dir),
+            2.0 * oc.dot(self.dir),
+            oc.dot(self.dir) - radius * radius,
+        );
+        let discriminant = b * b - 4.0 * a * c;
+        discriminant > 0.0
     }
 }
