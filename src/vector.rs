3ccfe0f01f35ee40a5560c6b4be529537b9f2a41
Jery Jacob Arangath <jerryjacobarangath@gmail.com>
2021-05-08 18:42:53 +0530

    Added tests to vector.rs.

--- src/vector.rs
+++ src/vector.rs
@@ -1,6 +1,6 @@
 use std::ops;
 
-#[derive(Copy, Clone)]
+#[derive(Debug, PartialEq, Copy, Clone)]
 pub struct Vec3 {
     x: f64,
     y: f64,
@@ -113,5 +113,59 @@ impl ops::Div<f64> for Vec3 {
             y: self.y / rhs,
             z: self.z / rhs,
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn vec3_test() {
+        let vec3 = Vec3::new(0.126, 0.314, 0.631);
+        let expected = (0.126, 0.314, 0.631);
+        assert_eq!((vec3.x, vec3.y, vec3.z), expected);
+    }
+
+    #[test]
+    fn vec3_length_squared_test() {
+        let vec3 = Vec3::new(0.126, 0.314, 0.631);
+        let expected = 0.512633;
+        assert_eq!(vec3.length_squared(), expected);
+    }
+
+    #[test]
+    fn vec3_length_test() {
+        let vec3 = Vec3::new(0.126, 0.314, 0.631);
+        let expected = 0.71598393836733516490216633646487;
+        assert_eq!(vec3.length(), expected);
+    }
+
+    #[test]
+    fn vec3_unit_vector_test() {
+        let vec3 = Vec3::new(0.126, 0.314, 0.631);
+        let expected = Vec3::new(
+            0.17598160132938032, // Round-Off error detected. Probably issue with type system 0.1759816013293803[3]793702894350261
+            0.4385573239478208421605324465065,
+            0.88130468602253169236718462976307,
+        );
+        assert_eq!(Vec3::unit_vector(vec3), expected);
+    }
+
+    #[test]
+    fn vec3_dot_test() {
+        let vec1 = Vec3::new(0.126, 0.314, 0.631);
+        let vec2 = Vec3::new(0.314, 0.631, 0.126);
+        let expected = 0.317204_00000000004; // Multiplication Overflow detected, '_' used as demarcation.
+        assert_eq!(vec1.dot(vec2), expected);
+    }
+
+    #[test]
+    fn vec3_cross_test() {
+        let vec1 = Vec3::new(0.126, 0.314, 0.631);
+        let vec2 = Vec3::new(0.314, 0.631, 0.126);
+
+        let expected =Vec3::new(-0.358597, 0.182258, -0.019089999999999996); // Multiplication Overflow detected, z vector erroneous. (CALCULATED:-0.01909)
+        assert_eq!(vec1.cross(vec2), expected);
     }
 }
