b77b7fbbe1ac0e2a2c056954dede846ab2a04f1a
Devdutt Shenoi <devdutt@outlook.in>
2021-05-15 17:51:35 +0530

    Fix anti-aliasing

--- examples/example.rs
+++ examples/example.rs
@@ -4,8 +4,8 @@ use std::{
 };
 
 use rtrcrs::{
-    camera::Camera, definitions::random_double, hittable_list::HittableList, ray::Point3,
-    sphere::Sphere,
+    camera::Camera, color::Color, definitions::random_double, hittable_list::HittableList,
+    ray::Point3, sphere::Sphere,
 };
 
 fn main() {
@@ -13,6 +13,7 @@ fn main() {
     const ASPECT_RATIO: f64 = 16.0 / 9.0;
     const IMAGE_WIDTH: i32 = 400;
     const IMAGE_HEIGHT: i32 = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as i32;
+    const SAMPLES_PER_PIXEL: i32 = 100;
 
     //World
     let mut world = HittableList::default();
@@ -29,12 +30,15 @@ fn main() {
         eprint!("\rScanlines remaining: {} ", j);
         stderr().flush().unwrap();
         for i in 0..IMAGE_WIDTH {
-            let (u, v) = (
-                (i as f64 + random_double()) / (IMAGE_WIDTH - 1) as f64,
-                (j as f64 + random_double()) / (IMAGE_HEIGHT - 1) as f64,
-            );
-            let pixel_color = camera.get_ray(u, v).color(&world);
-            println!("{}", pixel_color);
+            let mut pixel_color = Color::new(0.0, 0.0, 0.0);
+            for _ in 0..SAMPLES_PER_PIXEL {
+                let (u, v) = (
+                    (i as f64 + random_double()) / (IMAGE_WIDTH - 1) as f64,
+                    (j as f64 + random_double()) / (IMAGE_HEIGHT - 1) as f64,
+                );
+                pixel_color += camera.get_ray(u, v).color(&world);
+            }
+            println!("{}", pixel_color.anti_aliased(SAMPLES_PER_PIXEL));
         }
     }
 
--- src/color.rs
+++ src/color.rs
@@ -3,6 +3,14 @@ use std::fmt;
 /// Defines a pixel's color as an RGB value.
 pub type Color = crate::Vec3;
 
+impl Color {
+    pub fn anti_aliased(&self, samples_per_pixel: i32) -> Self {
+        let sample = |s: f64| (s / samples_per_pixel as f64).max(0.0).min(0.999);
+
+        Self::new(sample(self.x()), sample(self.y()), sample(self.z()))
+    }
+}
+
 impl fmt::Display for Color {
     /// Write the translated \[0,255\] value of each Color.
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
