fdcb14b8c72a8ff33da72a310a8aa1aae77a8bc5
bkp31415 <m.bharathkrishna07@gmail.com>
2021-05-19 22:09:38 +0530

    Fix closest_so_far bug in hittable_list.rs

--- src/hittable_list.rs
+++ src/hittable_list.rs
@@ -32,7 +32,7 @@ impl Hittable for HittableList {
         let mut closest_so_far = t_max;
 
         for object in self.objects.iter() {
-            if object.hit(ray, t_min, t_max, &mut temp_rec) {
+            if object.hit(ray, t_min, closest_so_far, &mut temp_rec) {
                 hit_anything = true;
                 closest_so_far = temp_rec.t;
                 rec.set(&temp_rec);
